e33c46702922884485b6f2ea19687e620080e168
Ubuntu <ubuntu@ip-172-31-28-204.eu-west-1.compute.internal>
2021-04-15 14:21:40 +0000

    sets database to users

--- backend/application/__init__.py
+++ backend/application/__init__.py
@@ -4,9 +4,9 @@ from os import getenv
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = str(getenv('DATABASE_URI'))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@mysql:3306/users'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = str(getenv('SECRET_KEY'))
+app.config['SECRET_KEY'] = 'ncjksncnsd'
 db = SQLAlchemy(app)
 
 from application import routes
